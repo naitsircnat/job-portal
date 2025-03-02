b8b0afed045750888013a5b82110e91064d1ac03
naitsircnat <cristiangerardchen@gmail.com>
2025-03-02 06:11:58 +0000

    Added all classes, pending part-time and contract

--- Applicant.java
+++ Applicant.java
@@ -1,3 +1,76 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Applicant {
+  private String firstName;
+  private String lastName;
+  private int expectedSalary;
+  private List<String>education;
+  private List<String>experience;
+  private String earliestStartDate;
+
+  public Applicant(String firstName, String lastName){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.education=new ArrayList<String>();
+    this.experience=new ArrayList<String>();
+  }
+  
+  public String getFirstName(){
+    return firstName;
+  }
+
+  public void setFirstName(String firstName){
+    this.firstName=firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public int getExpectedSalary() {
+    return expectedSalary;
+  }
+
+  public void setExpectedSalary(int expectedSalary) {
+    this.expectedSalary = expectedSalary;
+  }
+
+  public List<String> getEducation() {
+    return education;
+  }
+
+  public void setEducation(List<String> education) {
+    this.education = education;
+  }
+
+  public List<String> getExperience() {
+    return experience;
+  }
+
+  public void setExperience(List<String> experience) {
+    this.experience = experience;
+  }
+
+  public String getEarliestStartDate() {
+    return earliestStartDate;
+  }
+
+  public void setEarliestStartDate(String earliestStartDate) {
+    this.earliestStartDate = earliestStartDate;
+  }
+
+  @Override
+  public String toString() {
+    return "Applicant [firstName=" + firstName + ", lastName=" + lastName + ", expectedSalary=" + expectedSalary
+        + ", education=" + education + ", experience=" + experience + ", earliestStartDate=" + earliestStartDate + "]";
+  }
+
+  
+
   
 }
--- Application.java
+++ Application.java
@@ -1,3 +1,43 @@
+import java.util.Date;
+
 public class Application {
+  private Job job;
+  private Applicant applicant;
+  private Date applicationDate;
   
+  public Application (Job job, Applicant applicant){
+    this.applicant=applicant;
+    this.job=job;
+    this.applicationDate=new Date();
+  }
+
+  public Job getJob() {
+    return job;
+  }
+
+  public void setJob(Job job) {
+    this.job = job;
+  }
+
+  public Applicant getApplicant() {
+    return applicant;
+  }
+
+  public void setApplicant(Applicant applicant) {
+    this.applicant = applicant;
+  }
+
+  public Date getApplicationDate() {
+    return applicationDate;
+  }
+
+  public void setApplicationDate(Date applicationDate) {
+    this.applicationDate = applicationDate;
+  }
+
+  @Override
+  public String toString() {
+    return "Application [job=" + job + ", applicant=" + applicant + ", applicationDate=" + applicationDate + "]";
+  }
+
 }
